872d2dc28f47eb7b0dbcc4d86f88b76565203735
MaloEce <malo.couturierkl@gmail.com>
2024-12-08 10:13:47 +0800

    Update main.c

--- ReseauxTrophiques/main.c
+++ ReseauxTrophiques/main.c
@@ -7,48 +7,67 @@ int main() {
 
     ReseauTrophique *reseau = chargerReseau(nomFichier);
 
-    int choix, iterations;
+    int choix, choix2;
     do {
         printf("\n--- Menu ---\n");
         printf("1. Afficher le reseau trophique\n");
-        printf("2. Rechercher les producteurs\n");
-        printf("3. Rechercher les consommateurs\n");
-        printf("4. Simuler la dynamique des populations\n");
-        printf("5. Afficher la complexitée du reseau\n");
-        printf("6. Verifier la connexite \n");
-        printf("7. Quitter\n");
-        printf("Votre choix : ");
+        printf("2. Rechercher les sommets particuliers\n");
+        printf("3. Afficher la complexitee du reseau\n");
+        printf("4. Quitter\n");
         scanf("%d", &choix);
 
         switch (choix) {
             case 1:
                 afficherReseau(reseau);
                 break;
+
             case 2:
-                rechercherMaillons(reseau, "producteur");
-                break;
+                choix=0;
+                do {
+                    printf("\n--- Recherche des sommets particuliers ---\n");
+                    printf("1. Rechercher les consommateurs\n");
+                    printf("2. Rechercher les producteurs\n");
+                    printf("3. Especes n'ayant qu'une source d'alimentation\n");
+                    printf("4. Rechercher tous les predecesseurs\n");
+                    printf("5. Revenir en arriere\n");
+                    scanf("%d", &choix2);
+                    switch (choix2) {
+                        case 1:
+                            rechercherMaillons(reseau, "consommateur");
+                            break;
+
+                        case 2:
+                            rechercherMaillons(reseau, "producteur");
+                            break;
+
+                        case 3:
+                            unpre(reseau);
+                            break;
+
+                        case 4:
+                            toutpre(reseau);
+                            break;
+
+                        case 5:
+                            break;
+
+                        default:
+                            printf("Choix invalide. Reessayez.\n");
+                    }
+                }while(choix2!=5);
+
             case 3:
-                rechercherMaillons(reseau, "consommateur");
-                break;
-            case 4:
-                printf("Entrez le nombre d'itérations : ");
-                scanf("%d", &iterations);
-                simulerDynamique(reseau, iterations);
-                break;
-            case 5:
                 afficherComplexite(reseau);
                 break;
-            case 6:
-                verifierConnexiteEtFiltrer(reseau);
-                break;
 
-            case 7:
+            case 4:
                 printf("Au revoir !\n");
                 break;
+
             default:
-                printf("Choix invalide. Réessayez.\n");
+                printf("Choix invalide. Reessayez.\n");
         }
-    } while (choix != 7);
+    } while (choix != 4);
 
     libererReseau(reseau);
     return 0;
